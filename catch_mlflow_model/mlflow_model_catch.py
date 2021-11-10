6cfa0e42acf1e25b8d2c9ce20fa9a71e4751fbea
Heiko Kulinna <Heikokulinna@gmx.de>
2021-11-10 22:44:32 +0100

    [HK] fix get_mlflow_model

--- catch_mlflow_model/mlflow_model_catch.py
+++ catch_mlflow_model/mlflow_model_catch.py
@@ -54,6 +54,7 @@ stage = "Staging"
 #pyfunc
 model = mlflow.pyfunc.load_model(model_uri= f"models:/{model_name}/{stage}")
 model_id = model.metadata.run_id
+model_id
 
 
 connection_string = os.getenv("AZURE_STORAGE_CONNECTION_STRING", "")
@@ -85,17 +86,25 @@ feature_limits
 import os
 from functools import lru_cache
 from pathlib import PurePosixPath
+from pathlib import PurePath
 from mlflow.pyfunc import load_model as ml_py_load
 # from mlflow.sklearn import load_model as ml_sk_load
 
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 @lru_cache(maxsize=None)
 def get_mlflow_model(model_name):
-    model_dir = os.getenv("MLFLOW_MODEL_DIRECTORY", "models:/")
+    # model_name = "dashapp_model"
+    model_dir = os.getenv("MLFLOW_MODEL_DIRECTORY", "models:")
     model_stage = os.getenv("MLFLOW_MODEL_STAGE", "Staging")
 
+    #f"models:/{model_name}/{stage}"
+
     artifact_path = PurePosixPath(model_dir).joinpath(model_name, model_stage)
+    print(artifact_path)
 
     model = ml_py_load(str(artifact_path))
 
